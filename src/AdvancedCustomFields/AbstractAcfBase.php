fe6ecdd61766da217c958f789d453ef46424fe75
Emanuel Lopes <emanuel.lopes.pt@gmail.com>
2022-08-04 14:23:07 +0100

    feat: add function for create ption page and form inside option page

--- src/AdvancedCustomFields/AbstractAcfBase.php
+++ src/AdvancedCustomFields/AbstractAcfBase.php
@@ -17,4 +17,22 @@ abstract class AbstractAcfBase extends AbstractFeature
 
         return $fieldsBuilder;
     }
+
+    /**
+     * @param  array  $options ACFPage Options
+     * @param  string  $name Name of form inside options page
+     *
+     * @return FieldsBuilder
+     */
+    protected function createACFFOptionsPage(array $options, string $name): FieldsBuilder
+    {
+        $fieldsBuilder = new FieldsBuilder($name);
+
+        add_action('acf/init', function () use ($options, $fieldsBuilder) {
+            acf_add_options_page($options);
+            acf_add_local_field_group($fieldsBuilder->build());
+        });
+
+        return $fieldsBuilder;
+    }
 }
