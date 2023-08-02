ef1e76be78780e9600e0589722bd30cc74293384
Emanuel Lopes <emanuel.lopes.pt@gmail.com>
2023-08-02 22:46:09 +0100

    fix: removed unused code

--- src/Models/BasePost.php
+++ src/Models/BasePost.php
@@ -2,39 +2,9 @@
 
 namespace Snape\EcoSystemWP\Models;
 
-use Timber\Image;
 use Timber\Post;
 
 class BasePost extends Post
 {
-    /**
-     * @param  int|null  $image_id
-     *
-     * @return Image|null
-     */
-    protected function getImageObject(?int $image_id): ?Image
-    {
-        if (! $image_id) {
-            return null;
-        }
 
-        return new Image($image_id);
-    }
-
-    /**
-     * This returns the saved title of the article,
-     * This function doesn't work  if you want to preview the unsaved title.
-     *
-     * @return string the title of the article.
-     */
-    public function title(): string
-    {
-        remove_filter('the_title', 'wptexturize');
-        remove_filter('the_title', 'convert_chars');
-        $title = apply_filters('the_title', $this->post_title, $this->ID); // @phpstan-ignore-line
-        add_filter('the_title', 'wptexturize');
-        add_filter('the_title', 'convert_chars');
-
-        return $title;
-    }
 }
--- src/Models/BaseTerm.php
+++ src/Models/BaseTerm.php
@@ -7,30 +7,4 @@ use Timber\Term;
 
 class BaseTerm extends Term
 {
-    public string $slug;
-
-    /**
-     * This returns the saved title of the article,
-     * This function doesn't work  if you want to preview the unsaved title.
-     *
-     * @return string the title of the article.
-     */
-    public function title(): string
-    {
-        return wp_specialchars_decode($this->name);
-    }
-
-    /**
-     * @param  int|null  $image_id
-     *
-     * @return Image|null
-     */
-    protected function getImageObject(?int $image_id): ?Image
-    {
-        if (! $image_id) {
-            return null;
-        }
-
-        return new Image($image_id);
-    }
 }
